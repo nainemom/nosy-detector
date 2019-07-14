520c05b51ea57b95a2299a7967f5bd10abd0f731
nainemom <nainemom@gmail.com>
2019-07-14 16:35:43 +0430

    use motion

--- src/lib/webcam.js
+++ src/lib/webcam.js
@@ -1,17 +1,42 @@
-import NodeWebcam from 'node-webcam'
+import { exec, spawn } from 'child_process'
+import path from 'path'
+import fs from 'fs'
 
 export default {
-  getPicture() {
+  process: undefined,
+  start() {
+    const motionConfig = path.join(__dirname, '../../config/motion.conf')
     return new Promise((resolve, reject) => {
-      NodeWebcam.capture('test', {
-        callbackReturn: 'buffer',
-        saveShots: true
-      }, function( err, data ) {
-        if (err) {
-          return reject(err)
-        }
-        return resolve(data)
-      })
+      this.process = spawn('motion', ['-c', motionConfig])
+      process.nextTick(resolve)
     })
+  },
+  stop() {
+    this.process.kill()
+  },
+  getPicture() {
+    const picAddress = path.join(__dirname, '../../camera/img.jpg')
+    return new Promise(resolve => {
+      const loop = () => {
+        if (fs.existsSync(picAddress)) {
+          return resolve(picAddress)
+        }
+        setTimeout(loop, 500)
+      }
+      loop()
+    })
+    
+
+    // return new Promise((resolve, reject) => {
+    //   NodeWebcam.capture('test', {
+    //     callbackReturn: 'buffer',
+    //     saveShots: true
+    //   }, function( err, data ) {
+    //     if (err) {
+    //       return reject(err)
+    //     }
+    //     return resolve(data)
+    //   })
+    // })
   }
 }
