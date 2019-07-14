02c45e11d7d318085f57a3eb63cb256c320bdecf
Amir Momenian <nainemom@gmail.com>
2019-07-15 00:37:08 +0430

    get webcam device from args

--- src/lib/webcam.js
+++ src/lib/webcam.js
@@ -1,13 +1,13 @@
 import { exec, spawn } from 'child_process'
 import path from 'path'
 import fs from 'fs'
-const cameraPicAddress = path.join(__dirname, '../../camera/img.jpg')
+const cameraPicAddress = path.join(__dirname, '../../camera_temp.jpg')
 
 export default {
   process: undefined,
-  start() {
+  start(device) {
     return new Promise((resolve, reject) => {
-      this.process = spawn('ffmpeg', ['-y', '-i', '/dev/video0', '-update', '1', '-r', '1', '-s', '800x600', cameraPicAddress])
+      this.process = spawn('ffmpeg', ['-y', '-i', device, '-update', '1', '-r', '1', '-s', '800x600', cameraPicAddress])
       setTimeout(resolve, 2000)
     })
   },
