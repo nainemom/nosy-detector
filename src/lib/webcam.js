f279b505adf33d4d6f1f1ca7c84acb11e0685dcc
nainemom <nainemom@gmail.com>
2019-07-14 18:18:00 +0430

    use ffmpeg

--- src/lib/webcam.js
+++ src/lib/webcam.js
@@ -1,42 +1,20 @@
 import { exec, spawn } from 'child_process'
 import path from 'path'
 import fs from 'fs'
+const cameraPicAddress = path.join(__dirname, '../../camera/img.jpg')
 
 export default {
   process: undefined,
   start() {
-    const motionConfig = path.join(__dirname, '../../config/motion.conf')
     return new Promise((resolve, reject) => {
-      this.process = spawn('motion', ['-c', motionConfig])
-      process.nextTick(resolve)
+      this.process = spawn('ffmpeg', ['-y', '-i', '/dev/video0', '-update', '1', '-r', '1', '-s', '800x600', cameraPicAddress])
+      setTimeout(resolve, 2000)
     })
   },
   stop() {
     this.process.kill()
   },
   getPicture() {
-    const picAddress = path.join(__dirname, '../../camera/img.jpg')
-    return new Promise(resolve => {
-      const loop = () => {
-        if (fs.existsSync(picAddress)) {
-          return resolve(picAddress)
-        }
-        setTimeout(loop, 500)
-      }
-      loop()
-    })
-    
-
-    // return new Promise((resolve, reject) => {
-    //   NodeWebcam.capture('test', {
-    //     callbackReturn: 'buffer',
-    //     saveShots: true
-    //   }, function( err, data ) {
-    //     if (err) {
-    //       return reject(err)
-    //     }
-    //     return resolve(data)
-    //   })
-    // })
+    return cameraPicAddress
   }
 }
