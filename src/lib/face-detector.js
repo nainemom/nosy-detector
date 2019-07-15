e81c3b31b347fec27f5323b76ab62a84dc7ba833
Amir Momenian <nainemom@gmail.com>
2019-07-15 19:49:55 +0430

    paint ellipse on faces

--- src/lib/face-detector.js
+++ src/lib/face-detector.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default {
   FACE_CASCADE: undefined,
   init: function () {
-    this.FACE_CASCADE = path.join(__dirname, '../../cascade/haarcascade_frontalface_default.xml')
+    this.FACE_CASCADE = path.join(__dirname, '../../cascade/haarcascade_frontalface_alt_tree.xml')
   },
   detect: function (media) {
     return new Promise((resolve, reject) => {
@@ -17,7 +17,14 @@ export default {
           if (err) {
             return reject(err)
           }
-          resolve(faces.length)
+          for (let i = 0; i < faces.length; i++){
+            const x = faces[i]
+            im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+          }
+          resolve({
+            faces: faces.length,
+            im
+          })
         })
       })
     })
