fb806fb50bff61d18a05f59c18ed9a98c955daa3
Amir Momenian <nainemom@gmail.com>
2019-07-16 01:58:28 +0430

    use node-cmd change usage add man page

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@
 import argv from './lib/argv.js'
 import webcam from './lib/webcam.js'
 import faceDetector from './lib/face-detector.js'
-import fs from 'fs'
+import cmd from 'node-cmd'
 import path from 'path'
-import { exec } from 'child_process'
 
-const availablePersons = argv.is('--max') ? parseInt(argv.get('--max')) : 1
-const moreCommand = argv.get('--more-command') || 'pwd'
-const normalCommand = argv.get('--normal-command') || 'pwd'
-const device = argv.get('--device') || '/dev/video0'
-const outputdir = argv.get('--output-dir') || './'
-const debugMode = argv.is('--debug') || false
+let availablePersons = parseInt(argv.get('--max-faces') || argv.get('-mf') || 1)
+const command = argv.get('--command') || argv.get('-c') || 'pwd'
+const device = argv.get('--device') || argv.get('-d') || '/dev/video0'
+const outputdir = argv.get('--output-dir') || argv.get('-o') || './'
+const debugMode = argv.is('--debug') || argv.is('-t') || false
 const absoluteOutputDir = path.join(process.cwd(), outputdir)
 
 function main() {
   console.log(Date(), 'Nosy-Detector Started...')
 
   let _faces = undefined
+  let runningCmd = undefined
   let tryOver = {
     length: undefined,
     times: 0
@@ -40,11 +39,11 @@ function main() {
           // set trusted total faces
           _faces = faces
           console.log(Date(), `${faces} Face(s) Detected!`)
-          im.save(path.join(outputdir, `${Date.now()}-${faces}.jpg`))
+          im.save(path.join(absoluteOutputDir, `${Date.now()}-${faces}.jpg`))
           if (faces > availablePersons) {
-            exec(moreCommand)
+            runningCmd = cmd.get(command)
           } else {
-            exec(normalCommand)
+            cmd.get(`kill ${runningCmd.pid + 1}`)
           }
         }
       } else {
@@ -56,5 +55,20 @@ function main() {
   })
 }
 
-main()
-process.stdin.resume()
+if (argv.is('--help') || argv.is('-h')) {
+  console.log('')
+  console.log('Options:')
+  console.log('--max-faces -mf     ', 'Maximum faces available behind system (default 1)')
+  console.log('--command -c        ', 'User command that runs when faces more than --max-faces (default "pwd")')
+  console.log('--device -d         ', 'Camera device address (default /dev/video0)')
+  console.log('--output-dir -o     ', 'Directory to save photos of face changes (default ./)')
+  console.log('--debug -t          ', 'Using for debug')
+  console.log('--help -h           ', 'Show man page')
+  console.log('')
+  console.log('Example Usage:')
+  console.log('nosy-detector -mf 1 -o ./saved_pics" -c "eog fuckoff.png"')
+  console.log('')
+} else {
+  main()
+  process.stdin.resume()
+}
