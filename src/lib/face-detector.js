d125570afdf71a93fdea635a03a138ec84d0a336
nainemom <nainemom@gmail.com>
2019-07-15 15:18:29 +0430

    use manual face cascade

--- src/lib/face-detector.js
+++ src/lib/face-detector.js
@@ -1,14 +1,19 @@
 import cv from 'opencv'
+import fs from 'fs'
 import path from 'path'
 
 export default {
+  FACE_CASCADE: undefined,
+  init: function () {
+    this.FACE_CASCADE = path.join(__dirname, '../../cascade/haarcascade_frontalface_default.xml')
+  },
   detect: function (media) {
     return new Promise((resolve, reject) => {
-      cv.readImage(media, function(err, im){
+      cv.readImage(media, (err, im) => {
         if (err) {
           return reject(err)
         }
-        im.detectObject(cv.FACE_CASCADE, {}, (err, faces) => {
+        im.detectObject(this.FACE_CASCADE, {}, (err, faces) => {
           if (err) {
             return reject(err)
           }
